ef32d8be22a420dc2c47640f4b5b01f049970bce
Trần Hoàng Nhã <thediffest@gmail.com>
2021-07-29 12:47:34 +0700

    add scripts

---
+++ scripts/createScreen.js
@@ -0,0 +1,47 @@
+// import fs from 'fs';
+const fs = require('fs');
+
+// import path from 'path';
+const path = require('path');
+
+const appPath = process.argv[2];
+const screenName = process.argv[3];
+const routeName = process.argv[4] ?? `${appPath}.${screenName.toLowerCase()}`;
+const dir = path.resolve(__dirname, `../src/${appPath}/${screenName}`);
+
+const index = `import React from 'react';
+import { createScreenComponent } from '@shared/helpers/createScreenComponent';
+import { Layout } from './Layout';
+import { useProps } from './useProps';
+
+export const ${screenName} = createScreenComponent(
+  '${routeName}',
+  (props) => (<Layout {...useProps(props)} />,
+  {}
+  ));
+`;
+
+const layout = `import React from 'react';
+import { View, StyleSheet } from 'react-native';
+export const Layout = () => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+`;
+
+const useProps = `export const useProps = (_params) => {
+  return {};
+};
+`;
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+fs.writeFileSync(dir + '/index.js', index);
+fs.writeFileSync(dir + '/Layout.js', layout);
+fs.writeFileSync(dir + '/useProps.js', useProps);
---
+++ scripts/document.md
@@ -0,0 +1,43 @@
+## Script Use
+
+### Create Screen
+
+yarn new:screen [1] [2] [3]
+
+### Create Page
+
+yarn new:page [path] [name]
+
+[path] : retailer/RTCustomScreen/pages
+[name]: EditCustomerPage
+
+=> yarn new:page retailer/RTCustomerScreen/pages EditCustomerPage name
+
+eg: yarn new:page retailer/RTReportsScreen/pages ReportSalesPage retailer.report.sales
+
+settings
+yarn new:page retailer/RTSettingsScreen/pages SettingStaffPage retailer.settings.staff
+yarn new:page retailer/RTSettingsScreen/pages SettingNewAttributesPage retailer.settings.attributes.new
+
+// home pages
+yarn new:page retailer/RTHomeScreen/pages HomeOrderListPage retailer.home.order.list
+yarn new:page retailer/RTHomeScreen/pages HomeOrderDetailPage retailer.home.order.detail
+
+### Create Component
+
+yarn new:component [name]
+
+- name : name of component
+
+=> yarn new:component Button
+
+### Create API
+
+yarn new:api [path] [name]
+
+- path : retailer / merchant / app
+- name : name of api \* upercase first letter
+
+=> yarn new:api retailer Attributes
+
+### Create Redux
