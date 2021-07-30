e7dbe281cb4b269429ea475b10a46f5cfe28b4ed
Trần Hoàng Nhã <thediffest@gmail.com>
2021-07-30 09:12:36 +0700

    run iPhone 12

--- src/screens/LoginScreen/Layout.js
+++ src/screens/LoginScreen/Layout.js
@@ -1,4 +1,4 @@
-import { images, layouts, textStyles, fonts } from '@shared/themes';
+import { images, layouts, textStyles, fonts, colors } from '@shared/themes';
 import React from 'react';
 import { Image, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 import { InputMerchantID } from './widgets';
@@ -19,9 +19,8 @@ export const Layout = ({
         <View style={layouts.marginVertical} />
         <Text style={textStyles.sf_pt_medium_17_500}>{t('Sign In')}</Text>
         <Image source={images.logo} style={styles.logo} resizeMode="contain" />
-        <View style={styles.inputContent}>
-          <InputMerchantID onChangeMID={onChangeMID} />
-        </View>
+        <View style={styles.marginHeight} />
+        <InputMerchantID onChangeMID={onChangeMID} />
         <View style={styles.marginHeight} />
         <Button
           width="100%"
@@ -47,6 +46,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: scaleWidth(16),
     paddingVertical: scaleHeight(50),
+    backgroundColor: colors.white,
   },
 
   logo: {
@@ -68,9 +68,10 @@ const styles = StyleSheet.create({
   },
 
   btnWhatIsMerchant: {
-    width: scaleWidth(110),
-    height: scaleHeight(16),
+    width: scaleWidth(200),
+    height: scaleHeight(25),
     marginTop: scaleHeight(18),
+    justifyContent: 'center',
   },
 
   txtWhatIsMerchant: {
