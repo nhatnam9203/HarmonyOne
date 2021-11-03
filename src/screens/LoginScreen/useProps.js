9bb7160cfa91463dcfa8a146b02e353e1050b12e
namphanlevinci <nam.phan@levincigroup.com>
2021-11-03 17:23:23 +0700

    get token

--- src/screens/LoginScreen/useProps.js
+++ src/screens/LoginScreen/useProps.js
@@ -5,6 +5,8 @@ import { ScreenNames } from '../ScreenName';
 import { auth } from '@redux/slices';
 import { useDispatch, useSelector } from 'react-redux';
 import AsyncStorage from '@react-native-community/async-storage';
+import messaging from '@react-native-firebase/messaging';
+
 
 export const useProps = (_params) => {
   const dispatch = useDispatch();
@@ -30,6 +32,31 @@ export const useProps = (_params) => {
       NavigationService.navigate(ScreenNames.PinScreen);
     },
   });
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    if (enabled) {
+      getFcmToken() //<---- Add this
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getFcmToken = async () => {
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+     console.log(fcmToken);
+     console.log("Your Firebase Token is:", fcmToken);
+    } else {
+     console.log("Failed", "No token received");
+    }
+  }
+
+  React.useState(()=>{
+    requestUserPermission();
+  },[]);
 
   React.useEffect(() => {
     setTextMessage(null);
