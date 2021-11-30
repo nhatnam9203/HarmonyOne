467fac8aaf9572291f8896e5637cb4cb19390abb
nam.phan@levinci <nam.phan@levinci.com>
2021-11-30 10:10:40 +0700

    link appointment to invoice

--- src/screens/InvoiceDetailScreen/useProps.js
+++ src/screens/InvoiceDetailScreen/useProps.js
@@ -1,10 +1,5 @@
 import React from "react";
-import { 
-  Alert, 
-  Platform, 
-  NativeEventEmitter,
-  NativeModules,
- } from "react-native";
+import { Alert, Platform } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import {
   useAxiosMutation,
@@ -12,14 +7,13 @@ import {
   changeStatustransaction,
   getAppointmentByDate,
   getInvoiceDetail,
+  getAppointmentById
 } from '@src/apis';
-import { dateToFormat, 
-  PaymentTerminalType, 
-  stringIsEmptyOrWhiteSpaces,
-  REMOTE_APP_ID,
-  APP_NAME,
-  POS_SERIAL,
- } from "@shared/utils";
+import {
+  dateToFormat,
+  PaymentTerminalType,
+  stringIsEmptyOrWhiteSpaces
+} from "@shared/utils";
 
 import { appointment, invoice } from "@redux/slices";
 import NavigationService from '@navigation/NavigationService'
@@ -31,31 +25,26 @@ import {
   requestTransactionDejavoo,
 } from "@utils";
 import { parseString } from 'react-native-xml2js';
-import configureStore from '@src/redux/store';
-const { clover } = NativeModules;
-const { persistor, store } = configureStore();
 
 export const useProps = (props) => {
   const dispatch = useDispatch();
   const [t] = useTranslation();
-
-   //ADD LISTENER FROM CLOVER MODULE
-   let eventEmitter = new NativeEventEmitter(clover);
-   let subscriptions = [];
 
   const viewShotRef = React.useRef();
   const popupProcessingRef = React.useRef();
   const invoiceRef = React.useRef(null);
   const popupConfirmPrintRef = React.useRef();
 
+  const isAppointmentDetail = props?.route?.params?.isAppointmentDetail;
+  const appointmentItem = props?.route?.params?.appointmentItem;
+
   const {
-    invoice: { invoiceDetail, isProcessVoidPaymentClover },
+    invoice: { invoiceDetail },
     appointment: { appointmentDate },
-    hardware: { 
+    hardware: {
       paymentMachineType,
-      dejavooMachineInfo,
-      cloverMachineInfo,
-     },
+      dejavooMachineInfo
+    },
   } = useSelector(state => state);
 
   const [, submitChangeStatusTransaction] = useAxiosMutation({
@@ -66,6 +55,9 @@ export const useProps = (props) => {
         // NavigationService.back();
         popupConfirmPrintRef?.current?.show();
         fetchInvoiceDetail();
+        if (isAppointmentDetail) {
+          fetchAppointmentById();
+        }
         // fetchAppointmentByDate();
       }
     },
@@ -89,116 +81,22 @@ export const useProps = (props) => {
     },
   });
 
-  /************************************* useEffect *************************************/
+  const [, fetchAppointmentById] = useAxiosQuery({
+    ...getAppointmentById(appointmentItem?.appointmentId),
+    queryId: "getAppointmentById_InvoiceDetail",
+    enabled: false,
+    onSuccess: (data, response) => {
+      if (response?.codeNumber == 200) {
+        dispatch(appointment.setAppointmentDetail(data));
+      }
+    },
+  });
 
-  React.useEffect(() => {
-    registerEvents()
-
-    return function cleanup() {
-      unregisterEvents();
-    };
-  }, []);
-
-  /****************** Integrate Clover **************************/
-
-  const registerEvents = () => {
-    clover.changeListenerStatus(true)
-    subscriptions = [
-      eventEmitter.addListener("voidPaymentSuccess", (data) => {
-        const { hardware: { cloverMachineInfo } } = store.getState();
-        dispatch(invoice.setIsProcessVoidPaymentClover(false));
-        let messageUpdate = {
-          ...data,
-          sn: _.get(cloverMachineInfo, "serialNumber"),
-        };
-        handleResultVoidTransactionCloverSuccess(messageUpdate);
-      }),
-      eventEmitter.addListener("voidPaymentFail", (data) => {
-        dispatch(invoice.setIsProcessVoidPaymentClover(false))
-        handleResultVoidTransactionCloverFailed(
-          _.get(data, "errorMessage")
-        );
-      }),
-      eventEmitter.addListener("refundPaymentSuccess", (data) => {
-        const { hardware: { cloverMachineInfo } } = store.getState();
-        dispatch(invoice.setIsProcessVoidPaymentClover(false))
-        let messageUpdate = {
-          ...data,
-          sn: _.get(cloverMachineInfo, "serialNumber"),
-        };
-        handleResultVoidTransactionCloverSuccess(messageUpdate);
-      }),
-      eventEmitter.addListener("refundPaymentFail", (data) => {
-        dispatch(invoice.setIsProcessVoidPaymentClover(false))
-        handleResultVoidTransactionCloverFailed(
-          _.get(data, "errorMessage")
-        );
-      }),
-      eventEmitter.addListener('pairingCode', data => {
-        const { invoice: { isProcessVoidPaymentClover } } = store.getState();
-        if(data){
-          if(isProcessVoidPaymentClover) {
-            popupProcessingRef?.current?.hide();
-          }
-        }
-      }),
-      eventEmitter.addListener('pairingSuccess', data => {
-    
-        const { invoice: { isProcessVoidPaymentClover } } = store.getState();
-        if(isProcessVoidPaymentClover) {
-          setTimeout(() => {
-            popupProcessingRef?.current?.show();
-          }, 200)
-        }
-       
-      }),
-
-      eventEmitter.addListener('deviceDisconnected', () => {
-        const { invoice: { isProcessVoidPaymentClover } } = store.getState();
-        if(isProcessVoidPaymentClover) {
-          dispatch(invoice.setIsProcessVoidPaymentClover(false))
-          handleResultVoidTransactionCloverFailed(
-            t("No connected device")
-          );
-        }
-      }),
-    ]
-  }
-
-  const unregisterEvents = () => {
-    subscriptions.forEach(e => e.remove())
-    subscriptions = []
-  }
-
-  const handleResultVoidTransactionCloverSuccess = async (result) => {
-    const { 
-      hardware: { cloverMachineInfo },
-      invoice: { invoiceDetail },
-     } = store.getState();
-
-    popupProcessingRef?.current?.hide();
-
-    const data = {
-      responseData: result,
-      paymentTerminal: "clover",
-    };
-    const body = await changeStatustransaction(invoiceDetail?.checkoutId, data);
-    submitChangeStatusTransaction(body.params);
-  };
-
-  const handleResultVoidTransactionCloverFailed = async (message) => {
-    popupProcessingRef?.current?.hide();
-    setTimeout(() => {
-      alert(message);
-    }, 300);
-  };
-
-  /****************** Function **************************/
 
   const handleResultRefundTransactionDejavoo = async (responses) => {
     popupProcessingRef?.current?.hide();
 
-    parseString(responses, async(err, result) => {
+    parseString(responses, async (err, result) => {
       if (err || _.get(result, "xmp.response.0.ResultCode.0") != 0) {
         let detailMessage = _.get(result, "xmp.response.0.RespMSG.0", "")
           .replace(/%20/g, " ");
@@ -212,7 +110,7 @@ export const useProps = (props) => {
         setTimeout(() => {
           alert(resultTxt);
         }, 300);
-          
+
       } else {
         const data = {
           responseData: responses,
@@ -225,141 +123,85 @@ export const useProps = (props) => {
   };
 
   const handleVoidRefundCreditCard = () => {
-      const paymentInformation =
-        invoiceDetail?.paymentInformation[0]?.responseData || {};
-      const method = _.get(
-        invoiceDetail,
-        "paymentInformation.0.paymentData.method"
-      );
+    const paymentInformation =
+      invoiceDetail?.paymentInformation[0]?.responseData || {};
+    const method = _.get(
+      invoiceDetail,
+      "paymentInformation.0.paymentData.method"
+    );
 
-      if (!_.isEmpty(paymentInformation)) {
-        popupProcessingRef?.current?.show();
-        
-        if (invoiceDetail?.status === "paid") {
-          if (paymentMachineType == PaymentTerminalType.Dejavoo) {
-            if (method != "Dejavoo") {
-              popupProcessingRef?.current?.hide();
-              alert(t("Your transaction is invalid"));
-              return;
-            }
-            const amount = _.get(
-              invoiceDetail,
-              "paymentInformation.0.amount"
-            );
+    if (!_.isEmpty(paymentInformation)) {
+      popupProcessingRef?.current?.show();
 
-            parseString(paymentInformation, (err, result) => {
-              if (err) {
-                setTimeout(() => {
-                  alert("Error");
-                }, 300);
-              } else {
-                const transactionId = _.get(result, "xmp.response.0.RefId.0");
-                const invNum = _.get(result, "xmp.response.0.InvNum.0");
-                const params = {
-                  tenderType: "Credit",
-                  transType: "Return",
-                  amount: parseFloat(amount).toFixed(2),
-                  RefId: transactionId,
-                  invNum: `${invNum}`,
-                  dejavooMachineInfo,
-                };
-                requestTransactionDejavoo(params).then((responses) => {
-                  handleResultRefundTransactionDejavoo(responses);
-                });
-              }
-            });
-          } else if (paymentMachineType == PaymentTerminalType.Clover) {
-            if (Platform.OS === 'ios') {
-              if (method != "Clover") {
-                popupProcessingRef?.current?.hide();
-                alert(t("Your transaction is invalid"));
-                return;
-              }
-              const portClover = _.get(cloverMachineInfo, "port")
-                ? _.get(cloverMachineInfo, "port")
-                : 80;
-              const ipClover = _.get(cloverMachineInfo, "ip");
-              const url = `wss://${ipClover}:${portClover}/remote_pay`;
-  
-              dispatch(invoice.setIsProcessVoidPaymentClover(true))
-  
-              const paymentInfo = {
-                url,
-                remoteAppId: REMOTE_APP_ID,
-                appName: APP_NAME,
-                posSerial: POS_SERIAL,
-                token: _.get(cloverMachineInfo, "token")
-                  ? _.get(cloverMachineInfo, "token", "")
-                  : "",
-                orderId: paymentInformation?.orderId || "",
-                paymentId: paymentInformation?.id || "",
-              };
-              clover.refundPayment(paymentInfo);
-            }
+      if (invoiceDetail?.status === "paid") {
+        if (paymentMachineType == PaymentTerminalType.Dejavoo) {
+          if (method != "Dejavoo") {
+            popupProcessingRef?.current?.hide();
+            alert(t("Your transaction is invalid"));
+            return;
           }
-            
-        } else if (invoiceDetail?.status === "complete") {
-          if (paymentMachineType == PaymentTerminalType.Dejavoo) {
-            if (method != "Dejavoo") {
-              popupProcessingRef?.current?.hide();
-              alert(t("Your transaction is invalid"));
-              return;
-            }
-            const amount = _.get(
-              invoiceDetail,
-              "paymentInformation.0.amount"
-            );
-            parseString(paymentInformation, (err, result) => {
-              if (err) {
-                setTimeout(() => {
-                  alert("Error");
-                }, 300);
-              } else {
-                const transactionId = _.get(result, "xmp.response.0.RefId.0");
-                const invNum = _.get(result, "xmp.response.0.InvNum.0");
-                const params = {
-                  tenderType: "Credit",
-                  transType: "Void",
-                  amount: parseFloat(amount).toFixed(2),
-                  RefId: transactionId,
-                  invNum: `${invNum}`,
-                  dejavooMachineInfo,
-                };
-                requestTransactionDejavoo(params).then((responses) => {
-                  handleResultRefundTransactionDejavoo(responses);
-                });
-              }
-            });
-          } else if (paymentMachineType == PaymentTerminalType.Clover) {
-            if (Platform.OS === 'ios') {
-              if (method != "Clover") {
-                popupProcessingRef?.current?.hide();
-                alert(t("Your transaction is invalid"));
-                return;
-              }
-              const portClover = _.get(cloverMachineInfo, "port") 
-                                ? _.get(cloverMachineInfo, "port") 
-                                : 80;
-              const ipClover = _.get(cloverMachineInfo, "ip");
+          const amount = _.get(
+            invoiceDetail,
+            "paymentInformation.0.amount"
+          );
 
-              const url = `wss://${ipClover}:${portClover}/remote_pay`;
-              dispatch(invoice.setIsProcessVoidPaymentClover(true))
-              const paymentInfo = {
-                url,
-                remoteAppId: REMOTE_APP_ID,
-                appName: APP_NAME,
-                posSerial: POS_SERIAL,
-                token: _.get(cloverMachineInfo, "token")
-                  ? _.get(cloverMachineInfo, "token", "")
-                  : "",
-                orderId: paymentInformation?.orderId || "",
-                paymentId: paymentInformation?.id || "",
+          parseString(paymentInformation, (err, result) => {
+            if (err) {
+              setTimeout(() => {
+                alert("Error");
+              }, 300);
+            } else {
+              const transactionId = _.get(result, "xmp.response.0.RefId.0");
+              const invNum = _.get(result, "xmp.response.0.InvNum.0");
+              const params = {
+                tenderType: "Credit",
+                transType: "Return",
+                amount: parseFloat(amount).toFixed(2),
+                RefId: transactionId,
+                invNum: `${invNum}`,
+                dejavooMachineInfo,
               };
-              clover.voidPayment(paymentInfo);
+              requestTransactionDejavoo(params).then((responses) => {
+                handleResultRefundTransactionDejavoo(responses);
+              });
             }
+          });
+        }
+      } else if (invoiceDetail?.status === "complete") {
+        if (paymentMachineType == PaymentTerminalType.Dejavoo) {
+          if (method != "Dejavoo") {
+            popupProcessingRef?.current?.hide();
+            alert(t("Your transaction is invalid"));
+            return;
           }
+          const amount = _.get(
+            invoiceDetail,
+            "paymentInformation.0.amount"
+          );
+          parseString(paymentInformation, (err, result) => {
+            if (err) {
+              setTimeout(() => {
+                alert("Error");
+              }, 300);
+            } else {
+              const transactionId = _.get(result, "xmp.response.0.RefId.0");
+              const invNum = _.get(result, "xmp.response.0.InvNum.0");
+              const params = {
+                tenderType: "Credit",
+                transType: "Void",
+                amount: parseFloat(amount).toFixed(2),
+                RefId: transactionId,
+                invNum: `${invNum}`,
+                dejavooMachineInfo,
+              };
+              requestTransactionDejavoo(params).then((responses) => {
+                handleResultRefundTransactionDejavoo(responses);
+              });
+            }
+          });
         }
       }
+    }
   }
 
   let isDebitPayment = false;
@@ -410,7 +252,7 @@ export const useProps = (props) => {
           viewShotRef?.current?.capture().then(uri => {
             RNFetchBlob.ios.previewDocument(uri);
           })
-      
+
         } else {
           viewShotRef?.current?.capture().then(async uri => {
             const shareResponse = await Share.open({
@@ -423,8 +265,9 @@ export const useProps = (props) => {
       }
     },
 
-    printInvoice : async() =>{
+    printInvoice: async () => {
       setTimeout(() => {
+        console.log('invoiceDetail', invoiceDetail)
         invoiceRef.current?.showAppointmentReceipt({
           appointmentId: invoiceDetail?.appointmentId,
           checkoutId: invoiceDetail?.checkoutId,
