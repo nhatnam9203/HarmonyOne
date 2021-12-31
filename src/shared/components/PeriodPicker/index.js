962a0f8a85aa52ca7671e7e6302fde583a9ee4f0
nam.phan@levinci <nam.phan@levinci.com>
2021-12-31 15:26:19 +0700

    fix

--- src/shared/components/PeriodPicker/index.js
+++ src/shared/components/PeriodPicker/index.js
@@ -215,7 +215,7 @@ export const PeriodPicker = ({
                     <DayPicker
                       dayPicked={endDate}
                       onApply={(date) => setEndDate(date)}
-                      minDate={moment(endDate).format("YYYY-MM-DD")}
+                      minDate={moment(startDate).format("YYYY-MM-DD")}
                       componentRender={() => (
                         <View style={styles.inputDate}>
                           <Text style={styles.txtInputDate}>{moment(endDate).format("MM/DD/YYYY")}</Text>
