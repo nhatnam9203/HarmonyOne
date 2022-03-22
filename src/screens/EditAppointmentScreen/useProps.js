8fce4fb975668a412cb5e4f8286a62b0cb5d6265
namphanlevinci <84962280+namphanlevinci@users.noreply.github.com>
2022-03-22 08:27:27 +0700

    shadow button

--- src/screens/EditAppointmentScreen/useProps.js
+++ src/screens/EditAppointmentScreen/useProps.js
@@ -310,10 +310,10 @@ export const useProps = (_params) => {
         fromTime: appointmentEdit.fromTime,
         status: appointmentEdit.status,
         categories: appointmentDetail.categories,
-        services: appointmentEdit.services.filter(sv => sv?.bookingServiceId),
-        extras: appointmentEdit.extras.filter(sv => sv?.bookingExtraId),
-        products: appointmentEdit.products.filter(sv => sv?.bookingProductId),
-        giftCards: appointmentEdit.giftCards.filter(sv => sv?.bookingGiftCardId),
+        services: appointmentDetail.services.filter(sv => sv?.bookingServiceId),
+        extras: appointmentDetail.extras.filter(sv => sv?.bookingExtraId),
+        products: appointmentDetail.products.filter(sv => sv?.bookingProductId),
+        giftCards: appointmentDetail.giftCards.filter(sv => sv?.bookingGiftCardId),
       };
 
       const body = await updateAppointment(appointmentEdit.appointmentId, data);
