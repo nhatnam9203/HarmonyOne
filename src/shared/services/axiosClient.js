72246b493f127a5c5e83a2a44cce43b1b1e1676e
hangkimduyen <hangkimduyen@gmail.com>
2022-03-25 08:54:32 +0700

    change file sound

--- src/shared/services/axiosClient.js
+++ src/shared/services/axiosClient.js
@@ -13,8 +13,8 @@ const log = (obj, message = '') => {
 
 log(Configs, 'Configs');
 export const axios = Axios.create({
-  baseURL: Configs.API_URL,
-  // baseURL : `https://api.harmonypayment.com/api/`,
+  // baseURL: Configs.API_URL,
+  baseURL : `https://api2.harmonypayment.com/api/`,
   timeout: 30000,
   headers: {
     Accept: 'application/json',
